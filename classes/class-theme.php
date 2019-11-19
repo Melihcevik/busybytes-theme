9f2b6b5c1795eef2c22feee4a07761c344665e50
Santiago Degetau Sada <santiago.degetau@icloud.com>
2019-11-18 18:36:58 -0600

    disable settings options page; add class-tgm-plugin-activation

--- classes/class-theme.php
+++ classes/class-theme.php
@@ -4,7 +4,7 @@
 *
 * @package BusyBytes
 * @subpackage BusyBytes Theme
-* @since BusyBytes Theme 1.0
+* @since 1.0.0
 */
 
 namespace BusyBytes;
@@ -21,10 +21,6 @@ if ( ! class_exists( 'Theme' ) ) {
 
 		// this class gets instantiated at "after-setup-theme" hook
 		public function __construct() {
-			$this->bb_theme_activate();
-		}
-
-		private function bb_theme_activate() {
 
 			// load the text domain (translations)
 			$this->bb_load_textdomain();
@@ -32,8 +28,13 @@ if ( ! class_exists( 'Theme' ) ) {
 			// check for compatibility and dependencies, store the result
 			$is_theme_compatible = $this->bb_theme_check_compatibility();
 
-			// load the settings class (if compatible)
-			if ( $is_theme_compatible ) require_once( 'class-settings.php' );
+			if ( $is_theme_compatible ) {
+				// fire up the tgm-plugin-activation class
+				// (checks for recommended and needed plugins)
+				require_once( get_template_directory() . '/includes/theme-plugins.php' );
+				// load the settings class [disabled for now]
+				// require_once( 'class-settings.php' ); 
+			}
 
 		}
 
@@ -56,7 +57,7 @@ if ( ! class_exists( 'Theme' ) ) {
 			if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
 				add_action( 'admin_notices', function() {
 					if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
-					$message = '<strong>'. self::THEME_NAME . '</strong> ' . __( 'requires <strong>Elementor</strong> version '. self::MINIMUM_ELEMENTOR_VERSION .' or greater.', 'bb-theme' );
+					$message = '<strong>' . self::THEME_NAME . '</strong> ' . __( 'requires <strong>Elementor</strong> version ' . self::MINIMUM_ELEMENTOR_VERSION . ' or greater.', 'bb-theme' );
 					printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 				});
 				return false;
@@ -66,7 +67,7 @@ if ( ! class_exists( 'Theme' ) ) {
 			if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
 				add_action( 'admin_notices', function() {
 					if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
-					$message = '<strong>'. self::THEME_NAME . '</strong> ' . __( 'requires <strong>PHP</strong> version '. self::MINIMUM_PHP_VERSION .' or greater.', 'bb-theme' );
+					$message = '<strong>' . self::THEME_NAME . '</strong> ' . __( 'requires <strong>PHP</strong> version ' . self::MINIMUM_PHP_VERSION . ' or greater.', 'bb-theme' );
 					printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 				});
 				return false;
@@ -76,7 +77,7 @@ if ( ! class_exists( 'Theme' ) ) {
 			if ( self::THEME_NAME != wp_get_theme()->parent_theme ) {
 				add_action( 'admin_notices', function() {
 					if ( isset( $_GET['activate'] ) ) unset( $_GET['activate'] );
-					$message = '<strong>'. self::THEME_NAME . '</strong> ' . __( 'requires a <strong>child theme</strong> to function properly. You should never edit a theme directly!', 'bb-theme' );
+					$message = '<strong>' . self::THEME_NAME . '</strong> ' . __( 'requires a <strong>child theme</strong> to function properly. You should never edit a theme directly!', 'bb-theme' );
 					printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 				});
 				return false;
---
+++ includes/theme-plugins.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * This file represents an example of the code that themes would use to register
+ * the required plugins.
+ *
+ * @see http://tgmpluginactivation.com/configuration/ for detailed documentation.
+ */
+
+require_once get_template_directory() . '/lib/TGM-Plugin-Activation/class-tgm-plugin-activation.php' ;
+
+add_action( 'tgmpa_register', 'bb_register_required_plugins' );
+
+/**
+ * Register the required plugins for this theme.
+ * This function is hooked into `tgmpa_register`, which is fired on the WP `init` action on priority 10.
+ */
+function bb_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+
+		array(
+			'name'      => 'SG Optimizer',
+			'slug'      => 'sg-cachepress',
+			'required'  => false,
+		),
+
+		array(
+			'name'      => 'Autoptimize',
+			'slug'      => 'autoptimize',
+			'required'  => false,
+		),
+
+		array(
+			'name'      => 'WP-SCSS',
+			'slug'      => 'wp-scss',
+			'required'  => false,
+		),
+		
+		array(
+			'name'      => 'Elementor',
+			'slug'      => 'elementor',
+			'required'  => false,
+		),
+
+		array(
+			'name'        => 'WordPress SEO by Yoast',
+			'slug'        => 'wordpress-seo',
+			'is_callable' => 'wpseo_init',
+			'required'    => false,
+		),
+
+		array(
+			'name'      => 'Duplicator – WordPress Migration Plugin',
+			'slug'      => 'duplicator',
+			'required'  => false,
+		),
+
+		array(
+			'name'      => 'Cookie Notice for GDPR',
+			'slug'      => 'cookie-notice',
+			'required'  => false,
+		),
+
+		array(
+			'name'      => 'reSmush.it Image Optimizer',
+			'slug'      => 'resmushit-image-optimizer',
+			'required'  => false,
+		),
+
+	);
+
+	/*
+	 * Array of configuration settings. Amend each line as needed.
+	 * Only uncomment the strings in the config array if you want to customize the strings.
+	 */
+	$config = array(
+		'id'           => 'bb-theme',              // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+	);
+
+	tgmpa( $plugins, $config );
+}
